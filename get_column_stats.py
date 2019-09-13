2ca0999606cc224b605328596d97c90d3d4700fc
Evan Lester <ETLester1@gmail.com>
2019-09-12 22:56:09 -0600

    fixed typos, added main, added exit codes

--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,89 @@
-import sys, math
+import sys
+import math
+import argparse
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
 
-f = open(file_name, 'r')
+def col_stats(data):
+    """Compute the mean and standard deviation
+    of a list of numbers
 
-V = []
+    Parameters
+    ----------
+    data : float
+        list of data points to calculate mean and stdev
+    Returns
+    -------
+    mean : float
+        The mean of data
+    stdev : float
+        The standard deviation of data
+    """
+    mean = sum(data)/len(data)
+    stdev = math.sqrt(sum([(mean-x)**2 for x in data]) / len(data))
+    return mean, stdev
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
 
-mean = sum(V)/len(V)
+def main():
+    """Compute the mean and stdev of a column in a matrix
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+    Parameters
+    ----------
+    file_name : array of int
+                Non-empty array containing numbers. The mean and standard
+                deviation will be calculated for indicated column
+   col_num : int
+            Integer indicating the column in file_name to
+            calculate the mean and standard deviation
+    Returns
+    -------
+    mean
+        Mean of the values in the indicated column in file_name
+    stdev
+        Standard deviation of the values in the indicated column in
+        file_name
+    """
+    parser = argparse.ArgumentParser(
+                description='Pass parameters',
+                prog='get_column_stats')
 
-print('mean:', mean)
-print('stdev:', stdev)
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='Name of the file',
+                        required=True)
+
+    parser.add_argument('--col_num',
+                        type=int,
+                        help='The column number',
+                        required=True)
+
+    args = parser.parse_args()
+    # determine if the file is present and if the correct
+    # permissions are present
+    try:
+        f = open(args.file_name, 'r')
+    except FileNotFoundError:
+        print >> sys.stderr, 'Could not find ' + args.file_name
+        sys.exit(1)
+    except PermissionError:
+        print >> sys.stderr, 'Could not open ' + args.file_name
+        sys.exit(1)
+
+    V = []
+
+    for l in f:
+        A = [int(x) for x in l.split()]
+        try:
+            V.append(A[args.col_num])
+        except IndexError:
+            # print('The column number is not within the dataframe')
+            print >> sys.stderr, 'column number not in dataframe'
+            sys.exit(1)
+
+    [mean, stdev] = col_stats(V)
+
+    print('mean:', mean)
+    print('stdev:', stdev)
+
+
+if __name__ == '__main__':
+    main()
